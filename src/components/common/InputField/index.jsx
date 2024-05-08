b3c9e6b2c58e5bd22338f19be78d893491be4861
Chxndxn <chandangowda.kr2@gmail.com>
2024-05-08 22:13:55 +0530

    [Component] Input field

---
+++ src/components/common/InputField/index.jsx
@@ -0,0 +1,50 @@
+/* Imports */
+import styles from "./index.style";
+import PropTypes from "prop-types";
+import { Box, TextField, Typography } from "@mui/material";
+
+const propTypes = {
+  label: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+  errorFlag: PropTypes.bool,
+  errMessage: PropTypes.string,
+};
+
+const InputField = ({
+  label,
+  placeholder,
+  type,
+  name,
+  value,
+  onChange,
+  errorFlag,
+  errMessage,
+}) => {
+  return (
+    <Box sx={styles.gapY}>
+      <Typography sx={styles.formLabel} color="text.secondary" gutterBottom>
+        {label}
+      </Typography>
+
+      <TextField
+        placeholder={placeholder}
+        variant="outlined"
+        sx={styles.fieldWidth}
+        size="small"
+        type={type}
+        name={name}
+        value={value}
+        onChange={(e) => onChange(name, e.target.value)}
+        error={errorFlag}
+        helperText={errMessage}
+      />
+    </Box>
+  );
+};
+
+InputField.propTypes = propTypes;
+export default InputField;
