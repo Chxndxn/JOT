7f0d357bd387efe450bc5799794992335233556d
Chxndxn <chandangowda.kr2@gmail.com>
2024-05-11 13:06:11 +0530

    [WIP] Auth page redesign
    Co-authored-by: Shashank <malfunctixn@gmail.com>
    Signed-off-by: Chxndxn <chandangowda.kr2@gmail.com>

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -4,6 +4,6 @@ export const headerNavElements = [
   { pageName: "Home", path: pages.root },
   { pageName: "About", path: pages.about.absolutePath },
   { pageName: "Contact", path: pages.contact.absolutePath },
-  { pageName: "Login", path: pages.login.absolutePath },
+  { pageName: "Login", path: pages.auth.absolutePath },
 ];
 export const footerNavElements = ["About", "Contact"];
